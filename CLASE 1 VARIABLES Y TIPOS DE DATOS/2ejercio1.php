24be78993ff2d11333c5b0c871e903b194abba71
Frankz Fabio Guevara Sechar <164808500+Frankz-Fabio-Guevara-Sechar@users.noreply.github.com>
2024-10-09 20:46:50 -0500

    Add files via upload

---
+++ CLASE 1 VARIABLES Y TIPOS DE DATOS/2ejercio1.php
@@ -0,0 +1,11 @@
+<?php
+$nombre="Fabio";
+$edad= 18;
+$altura=1.68;
+$tiene_mascota=true;
+
+echo"Mi nombre es: $nombre <br>";
+echo"Mi nombre es: $edad <br>";
+echo"Mi nombre es: $altura <br>";
+echo"tiene mascota ?: $tiene_mascota <br>";
+?>
---
+++ CLASE 1 VARIABLES Y TIPOS DE DATOS/VARIABLES Y TIPOS DE DATOS.php
@@ -0,0 +1,22 @@
+
+<?php
+$nombre = "Juan";  // Aquí estamos guardando el texto "Juan" en la variable $nombre
+$edad = 25;        // Aquí estamos guardando el número 25 en la variable $edad
+$altura = 1.75;    // Guardando un número decimal (flotante) en la variable $altura
+$es_estudiante = true;  // Guardando un valor booleano (verdadero/falso)
+?>
+
+
+<?php
+$nombre = "Ana";
+$edad = 30;
+$altura = 1.65;
+$es_programadora = true;
+
+echo "Nombre: $nombre<br>";
+echo "Edad: $edad<br>";
+echo "Altura: $altura metros<br>";
+echo "Es programadora: $es_programadora<br>";
+?>
+
+
